29fccd638121606455335fd70406889970bd5478
lijunhaoabroad <lijunhaoabroad@gmail.com>
2017-07-21 17:37:51 -0400

    basic interface

--- app/src/main/java/com/example/lijunhao/testapplication/CustomAdapter.java
+++ app/src/main/java/com/example/lijunhao/testapplication/CustomAdapter.java
@@ -6,6 +6,7 @@ package com.example.lijunhao.testapplication;
         import android.content.Context;
         import android.graphics.BitmapFactory;
         import android.support.v4.view.ViewPropertyAnimatorListener;
+        import android.support.v7.widget.CardView;
         import android.support.v7.widget.RecyclerView;
         import android.view.LayoutInflater;
         import android.view.View;
@@ -29,6 +30,7 @@ package com.example.lijunhao.testapplication;
 public class CustomAdapter extends BaseAdapter{
     Context c;
     ArrayList<Product> Products;
+    int number=0;
 
     public CustomAdapter(Context c, ArrayList<Product> Products) {
         this.c = c;
@@ -63,64 +65,68 @@ public class CustomAdapter extends BaseAdapter{
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+     //   ViewHolder holder;
         if(convertView==null)
        {
-           convertView= LayoutInflater.from(c).inflate(R.layout.list_item,parent,false);
-          holder =new ViewHolder();
-           holder.nameTxt=(TextView)convertView.findViewById(R.id.nameTxt);
-           holder.propTxt=(TextView)convertView.findViewById(R.id.price);
-           holder.descTxt=(TextView)convertView.findViewById(R.id.descTxt);
-           holder.img=(ImageView)convertView.findViewById(R.id.img);
-           holder.Qty=(TextView) convertView.findViewById(R.id.Qty);
-           holder.btnCart=(Button) convertView.findViewById(R.id.btnCart);
-           holder.btnAdd=(Button) convertView.findViewById(R.id.btnAddQty);
-           holder.btnReduce=(Button) convertView.findViewById(R.id.btnReduce);
-           convertView.setTag(holder);
-       }else{
-            holder=(ViewHolder) convertView.getTag();
-        }
+           convertView= LayoutInflater.from(c).inflate(R.layout.list_item,parent,false);}
+//          holder =new ViewHolder();
+//           holder.nameTxt=(TextView)convertView.findViewById(R.id.nameTxt);
+//           holder.propTxt=(TextView)convertView.findViewById(R.id.price);
+//           holder.descTxt=(TextView)convertView.findViewById(R.id.descTxt);
+//           holder.img=(ImageView)convertView.findViewById(R.id.img);
+//           holder.Qty=(TextView) convertView.findViewById(R.id.Qty);
+//           holder.btnCart=(Button) convertView.findViewById(R.id.btnCart);
+//           holder.btnAdd=(Button) convertView.findViewById(R.id.btnAddQty);
+////           holder.btnReduce=(Button) convertView.findViewById(R.id.btnReduce);
+//           convertView.setTag(holder);
+//       }else{
+//            holder=(ViewHolder) convertView.getTag();
+//        }
          //holder=convertView.getTag();
-//           TextView nameTxt= (TextView) convertView.findViewById(R.id.nameTxt);
-//           TextView propTxt= (TextView) convertView.findViewById(R.id.price);
-//           TextView descTxt= (TextView) convertView.findViewById(R.id.descTxt);
-//           ImageView img=(ImageView) convertView.findViewById(R.id.img);
-//           final TextView Qty=(TextView) convertView.findViewById(R.id.Qty);
-//           final Button btnCart=(Button) convertView.findViewById(R.id.btnCart);
-//           final Button btnAdd=(Button) convertView.findViewById(R.id.btnAddQty);
-//           final Button btnReduce=(Button) convertView.findViewById(R.id.btnReduce);
+           TextView nameTxt= (TextView) convertView.findViewById(R.id.nameTxt);
+           TextView propTxt= (TextView) convertView.findViewById(R.id.price);
+           TextView descTxt= (TextView) convertView.findViewById(R.id.descTxt);
+           ImageView img=(ImageView) convertView.findViewById(R.id.img);
+           final TextView Qty=(TextView) convertView.findViewById(R.id.Qty);
+           final Button btnCart=(Button) convertView.findViewById(R.id.btnCart);
+           final Button btnAdd=(Button) convertView.findViewById(R.id.btnAddQty);
+           final Button btnReduce=(Button) convertView.findViewById(R.id.btnReduce);
            final Product s= (Product) this.getItem(position);
-           holder.nameTxt.setText(s.getpName());
-           holder.propTxt.setText("Price: CAD$"+s.getpPrice());
-           holder.descTxt.setText(s.getpDesc());
-           Picasso.with(c).load(s.getpImage()).into(holder.img);
-//           holder.btnCart.setOnClickListener(new View.OnClickListener() {
-//               @Override
-//               public void onClick(View v) {
-//
-//                   holder.btnCart.setVisibility(v.GONE);
-//                   if(Integer.parseInt(Qty.getText().toString())==0)
-//                   Qty.setText("1");
-//               }
-//           });
-//           btnAdd.setOnClickListener(new View.OnClickListener() {
-//               @Override
-//               public void onClick(View v) {
-//               if(Integer.parseInt(Qty.getText().toString())>1)
-//                   Qty.setText(String.valueOf(Integer.parseInt(Qty.getText().toString())+1));
-//               }
-//           });
-//           btnReduce.setOnClickListener(new View.OnClickListener() {
-//               @Override
-//               public void onClick(View v) {
-//
-//                   Qty.setText(Integer.parseInt(Qty.getText().toString())-1);
-//                   if(Integer.parseInt(Qty.getText().toString())==0){
-//                       btnCart.setVisibility(v.VISIBLE);
-//                   }
-//               }
-//           });
-//
+           nameTxt.setText(s.getpName());
+           propTxt.setText("Price: CAD$"+s.getpPrice());
+           descTxt.setText(s.getpDesc());
+           Picasso.with(c).load(s.getpImage()).into(img);
+         btnCart.setOnClickListener(new CardView.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 btnCart.setVisibility(v.GONE);
+                 number=1;
+                 Qty.setText(String.valueOf(number));
+             }
+         });
+           btnAdd.setOnClickListener(new CardView.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   v.setTag(Qty.getText().toString());
+               if(v.getTag()!=null)
+                   number=Integer.parseInt(v.getTag().toString())+1;
+                   Qty.setText(String.valueOf(number));
+               }
+           });
+           btnReduce.setOnClickListener(new CardView.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   v.setTag(Qty.getText().toString());
+                   if(v.getTag()!=null){
+                       number=Integer.parseInt(v.getTag().toString())-1;
+                   Qty.setText(String.valueOf(number));
+               }
+                   if(number==0){
+                       btnCart.setVisibility(v.VISIBLE);
+                   }
+               }
+           });
+
 
 //      try{
 //            InputStream in = new java.net.URL(s.getpImage()).openStream();
